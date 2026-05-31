5d77744c8a6f4ba26861111f4eaed9626f0d5870
Muhammad Aldo <101037785+Reznaf@users.noreply.github.com>
2026-05-31 16:18:16 +0700

    Update Belajar sama shafa #2
    Rubah script

--- new-game-project/scenes/script/character/base/base_character.gd
+++ new-game-project/scenes/script/character/base/base_character.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-# parameters/idle/blend_position
-
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
@@ -21,6 +19,8 @@ func _physics_process(_delta):
 	update_animation_parameters(input_direction)
 	
 	velocity = input_direction * MOVE_SPEED
+	print("Input= ", input_direction)
+	print("Velocity= ", velocity)
 	
 	pick_new_state()
 	
@@ -30,7 +30,7 @@ func update_animation_parameters(move_input : Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/walk/blend_position", move_input)
 		animation_tree.set("parameters/idle/blend_position", move_input)
-
+		
 func pick_new_state():
 	if(velocity != Vector2.ZERO):
 		state_machine.travel("walk")
